8398749cbef834e31e47dc2283301c2de8c5e651
suziestephen <suzietstephen@gmail.com>
2021-04-24 15:38:51 +1000

    adding example burgers

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('McChicken', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', TRUE);
+
