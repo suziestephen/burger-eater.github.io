c873a942b1d3764b73fa8fffe8ed9ad7ab22b4a4
suziestephen <suzietstephen@gmail.com>
2021-04-24 15:40:09 +1000

    creating routes - get, post, put, delete

---
+++ controllers/burgersController.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+ 
+ 
+ // Import the model (burger.js) to use its database functions.
+const burger = require('../models/burger.js');
+
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const burgersObject = {
+        burgers: data,
+      };
+      res.render('index', burgersObject);
+    });
+});
+
+
+
+router.post('/api/burgers', (req, res) => {
+  burger.create(['burger_name'], req.body.burger_name, (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+
+
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+
+
+router.delete('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  burger.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+
+//Export routes for server.js to use.
+module.exports = router;
