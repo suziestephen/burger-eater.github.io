6c8c0ac84f137cdf71ced089d1c9dd40ccd07250
suziestephen <suzietstephen@gmail.com>
2021-04-30 08:59:13 +1000

    missing helper functions

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,40 @@
 // Import MySQL connection.
 const connection = require('./connection.js');
 
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = (num) => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+  const arr = [];
+
+ // Loop through the keys and push the key/value as a string int arr
+ for (const key in ob) {
+  let value = ob[key];
+  // Check to skip hidden properties
+  if (Object.hasOwnProperty.call(ob, key)) {
+    // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+    if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+      value = `'${value}'`;
+    }
+    // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+    // e.g. {sleepy: true} => ["sleepy=true"]
+    arr.push(`${key}=${value}`);
+  }
+}
+
+// Translate array of strings to a single comma-separated string
+return arr.toString();
+};
+
 
 
 // Object for all our SQL statement functions.
@@ -23,7 +57,7 @@ const orm = {
     queryString += cols.toString();
     queryString += ') ';
     queryString += 'VALUES (';
-    queryString += printQuestionMarks(vals.length);
+    queryString += printQuestionMarks(vals.length); 
     queryString += ') ';
 
     console.log(queryString);
@@ -41,7 +75,7 @@ const orm = {
     let queryString = `UPDATE ${table}`;
 
     queryString += ' SET ';
-    queryString += objToSql(objColVals);
+    queryString += objToSql(objColVals); 
     queryString += ' WHERE ';
     queryString += condition;
 
